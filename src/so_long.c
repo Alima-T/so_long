e2bd09c490d3acec1658501864ddadb2d0047a91
Alimat Okhapkina <aokhapki@2-A-6.42heilbronn.de>
2024-11-09 22:59:09 +0100

    add new functions

--- src/so_long.c
+++ src/so_long.c
@@ -6,96 +6,74 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:28:30 by aokhapki          #+#    #+#             */
-/*   Updated: 2024/11/09 22:50:48 by aokhapki         ###   ########.fr       */
+/*   Updated: 2024/11/09 22:56:11 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-# include "LIBFT/libft.h"
-# include "MLX42/include/MLX42/MLX42.h"
-# include "FT_PRINTF/ft_printf.h"
+#include "../so_long.h"
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-# include <fcntl.h>
-#include <stdbool.h>
-
-typedef struct s_player
+bool flood_fill(char **map, int x, int y, int *collectibles, bool *exit_found)
 {
-	int		x;
-	int		y;
-}		t_player;
+    if (map[y][x] == '1' || map[y][x] == 'V')
+		return false;  // '1' is wall, 'V' is visited
+    if (map[y][x] == 'C')
+		(*collectibles)--; 
+    if (map[y][x] == 'E')
+		*exit_found = true; 
+    map[y][x] = 'V';  
+    flood_fill(map, x + 1, y, collectibles, exit_found);
+    flood_fill(map, x - 1, y, collectibles, exit_found);
+    flood_fill(map, x, y + 1, collectibles, exit_found);
+    flood_fill(map, x, y - 1, collectibles, exit_found);
+    return true;
+}
 
-typedef struct s_img
+void validate_path(t_game *game/*char **map*/, int start_x, int start_y, int total_collectibles)
 {
-	mlx_image_t *ground_img;
-	mlx_image_t *wall_img;
-	mlx_image_t *exit_img;
-	mlx_image_t *player_img;
-	mlx_image_t *enemy_img;
-	mlx_image_t *collectable_img;
-}		 t_img;
+	int collectibles;
+	bool exit_found;
+	char **temp_map;
+	int	i;
+	
+	temp_map = malloc(sizeof(char *) * game->height_map);
+	if (!temp_map)
+		finish_game(game);
+	i = -1;
+	while (++i < game->height_map)
+		temp_map[i] = ft_strdup(game->map[i]);
+	collectibles = total_collectibles;
+	exit_found = false;	
+	flood_fill(temp_map, start_x, start_y, &collectibles, &exit_found);	
+	if (!(collectibles == 0 && exit_found))
+	{
+		ft_printf("Invalid paht\n");
+		free_temp_map(temp_map, game->height_map);
+		finish_game(game);
+	}
+	free_temp_map(temp_map, game->height_map);
+}
 
-
-typedef struct s_game
+int	main(int argc, char **argv)
 {
-	void		*mlx;
-	void		*win;
-	char		**map;
-	int			map_fd;
-	int			x;
-	int			y;
-	int			height_map;
-	int			width_map;
-	t_player	player;
-	mlx_image_t *img;
-	t_img		all_images;
-	int			col_count;
-	int 		steps;
-}			t_game;
+	t_game game;
+	
+	if(argc != 2)
+		return(1);
+	init_game(&game);
+	map_reader(&game, argv[1]);
+	check_validity(&game);
+	init_mlx(&game);
+	set_vars(&game); // later
+	validate_path(&game/*temp_map*/, game.player.x, game.player.y, game.col_count);
+	loops(&game);
+	finish_game(&game); // if someone clicked on X the game must exit in a clean way
+	return (0);
+}
 
-// init.c
-void 	init_mlx(t_game *game);
-void	init_textures(t_game *game);
-void count_vars(t_game *game, int *player_counter, int *exit_counter);
-// so_long.c
-void    loops(t_game *game);
-// draw.c
-void 	draw(void *para);
-void	draw_map(t_game *game);
-void	draw_img(t_game *game, mlx_image_t *img, int j, int i);
-// parsing.c
-void    map_reader(t_game *game, char *map);
-void    fill_real_map(t_game *game, char *reader);
-int 	is_only_spaces(char *str);
-// GNL
-char	*get_next_line(int fd);
-// int		mouse_exit(void);
-void	go_player(t_game *game, int key);
-// play.c
-int		key_press (t_game *game,int key);
-void	set_vars(t_game *game);
-void	init_game(t_game *game);
-void	draw_player(t_game *game);
-void	keyboard_control(void *parameter);
-
-void	img_to_img(mlx_image_t *dst, mlx_image_t *src, int x, int y);
-
-// check map
-void		check_walls(t_game *game);
-void	check_path(t_game *game, int new_y, int new_x, char c);
-void	keep_checking(t_game *game, int new_y, int new_x);
-
-// finish.c
-void	free_map(t_game *game, int line);
-void	finish(int exit_code);
-void	finish_game(t_game *game);
-void	check_validity(t_game *game);
-void	check_row_lengths(t_game *game);
-void	is_key_down(t_game *game, char *c);
-void	free_temp_map(char **temp_map,int line);
-
-#endif
+void	loops(t_game *game)
+{
+	mlx_loop_hook(game->mlx, draw, game);
+	mlx_loop_hook(game->mlx, keyboard_control, game);
+	mlx_loop(game->mlx);
+	mlx_terminate(game->mlx);
+}
